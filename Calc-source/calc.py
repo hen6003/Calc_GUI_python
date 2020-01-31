44495471ac90967814d5bf3820f3692c37e3f447
hen6003 <henrysmith6003@protonmail.com>
2020-01-31 22:14:25 +0000

    Add files via upload

--- Calc-source/calc.py
+++ Calc-source/calc.py
@@ -1,148 +1,169 @@
-#imports
-from tkinter import *
+# imports
+from tkinter import * # tkinter
 import tempfile
 import os
-import math
+import math # for math operations
 
-#creating root
+# creating root
 root = Tk()
 root.title("Calc")
-root.maxsize(318, 396)
-root.minsize(318, 396)
-dir_path = os.path.dirname(os.path.realpath(__file__)) #finding directory
-ver = "1.10" #version
-#images
+root.minsize(306, 390)
+root.maxsize(306, 390)
+dir_path = os.path.dirname(os.path.realpath(__file__)) # finding directory
+ver = "2.0" # version
+root.option_add('*font', ('comforta', 9, 'bold'))
+# images
 photo = PhotoImage(file = dir_path + "/Calc.png")
+settingicon = PhotoImage(file = dir_path + "/settings.png")
 root.iconphoto(True, photo)
 img = PhotoImage(file = dir_path + "/rounded_button.png")
 img_num = PhotoImage(file = dir_path + "/rounded_button_num.png")
 
-#functions
-def button_click(num): #writing numbers to entry box
-    current = e.get()
-    e.delete(0, END)
-    e.insert(0, str(current) + str(num))
+setting = Frame(root)
 
-def button_clear(): #clearing entry box
-    e.delete(0, END)
+# functions
+def button_click(num): # writing numbers to entry box
+    current = e.cget("text")
+    e.config(text = str(current) + str(num))
+
+def button_clear(): # clearing entry box
+    e.config(text = "")
 
 def button_add():
-    first_number = e.get()
+    first_number = e.cget("text")
     global func
     global f_num
     func = "add"
     f_num = float(first_number)
-    e.delete(0, END)
+    e.config(text = "")
 
 def button_minus():
-    first_number = e.get()
+    first_number = e.cget("text")
     global f_num
     global func
     func = "minus"
     f_num = float(first_number)
-    e.delete(0, END)
+    e.config(text = "")
 
 def button_divide():
-    first_number = e.get()
+    first_number = e.cget("text")
     global f_num
     global func
     func = "divide"
     f_num = float(first_number)
-    e.delete(0, END)
+    e.config(text = "")
 
 def button_times():
-    first_number = e.get()
+    first_number = e.cget("text")
     global f_num
     global func
     func = "times"
     f_num = float(first_number)
-    e.delete(0, END)
+    e.config(text = "")
 
-def button_equal(): #finds the answer
-    second_number = e.get()
-    e.delete(0, END)
+def button_equal(): # finds the answer
+    second_number = e.cget("text")
+    e.config(text = "")
 
     if func == "add":
-        e.insert(0, f_num + float(second_number))
+        e.config(text = f"{f_num + float(second_number):g}")
 
     if func == "minus":
-        e.insert(0, f_num - float(second_number))
+        e.config(text = f"{f_num - float(second_number):g}")
 
     if func == "divide":
         if float(second_number) == 0:
-            e.insert(0, "∞")
+            e.config(text = "inf")
 
         else:
-            e.insert(0, f_num / float(second_number))
+            e.config(text = f"{f_num / float(second_number):g}")
 
     if func == "times":
-        e.insert(0, f_num * float(second_number))
+        e.config(text = f"{f_num * float(second_number):g}")
 
-def square(): #squares number
-    div = float(e.get())
+def square(): # squares number
+    div = float(e.cget("text"))
     dived = div * div
-    e.delete(0, END)
-    e.insert(0, dived)
+    e.config(text = "")
+    e.config(text = f"{dived:g}")
 
-def rooting(): #square roots number
-    div = math.sqrt(float(e.get()))
-    e.delete(0, END)
-    e.insert(0, div)
+def rooting(): # square roots number
+    div = math.sqrt(float(e.cget("text")))
+    e.config(text = "")
+    e.config(text = f"{div:g}")
 
-#creating widgets
-frame = LabelFrame(root, text = "Version: " + str(ver))
-e = Entry(frame, width = 35, borderwidth = 3)
-button_1 = Button(frame, text = "1", padx = 0, pady = 0, command = lambda: button_click(1))
-button_2 = Button(frame, text = "2", padx = 0, pady = 0, command = lambda: button_click(2))
-button_3 = Button(frame, text = "3", padx = 0, pady = 0, command = lambda: button_click(3))
-button_4 = Button(frame, text = "4", padx = 0, pady = 0, command = lambda: button_click(4))
-button_5 = Button(frame, text = "5", padx = 0, pady = 0, command = lambda: button_click(5))
-button_6 = Button(frame, text = "6", padx = 0, pady = 0, command = lambda: button_click(6))
-button_7 = Button(frame, text = "7", padx = 0, pady = 0, command = lambda: button_click(7))
-button_8 = Button(frame, text = "8", padx = 0, pady = 0, command = lambda: button_click(8))
-button_9 = Button(frame, text = "9", padx = 0, pady = 0, command = lambda: button_click(9))
-button_0 = Button(frame, text = "0", padx = 0, pady = 0, command = lambda: button_click(0))
-button_equal = Button(frame, text = "=", padx = 0, pady = 0, command = button_equal)
-button_clear = Button(frame, text = "C", padx = 0, pady = 0, command = button_clear)
-button_add = Button(frame, text = "+", padx = 0, pady = 0, command = button_add)
-button_minus = Button(frame, text = "-", padx = 0, pady = 0, command = button_minus)
-button_divide = Button(frame, text = "/", padx = 0, pady = 0, command = button_divide)
-button_times = Button(frame, text = "*", padx = 0, pady = 0, command = button_times)
-button_square = Button(frame, text = "x²", padx = 0, pady = 0, command = square)
-button_root = Button(frame, text = "√", padx = 0, pady = 0, command = rooting)
+def settings():
+    def save(): # saving fuction
+        if optionVar.get() == "Left     ":
+            e.config(anchor = W)
 
-#coustom buttons
-button_1.config(image = img_num, compound = CENTER, relief = FLAT)
-button_2.config(image = img_num, compound = CENTER, relief = FLAT)
-button_3.config(image = img_num, compound = CENTER, relief = FLAT)
-button_4.config(image = img_num, compound = CENTER, relief = FLAT)
-button_5.config(image = img_num, compound = CENTER, relief = FLAT)
-button_6.config(image = img_num, compound = CENTER, relief = FLAT)
-button_7.config(image = img_num, compound = CENTER, relief = FLAT)
-button_8.config(image = img_num, compound = CENTER, relief = FLAT)
-button_9.config(image = img_num, compound = CENTER, relief = FLAT)
-button_0.config(image = img_num, compound = CENTER, relief = FLAT)
-button_clear.config(image = img, compound = CENTER, relief = FLAT)
-button_equal.config(image = img, compound = CENTER, relief = FLAT)
-button_add.config(image = img, compound = CENTER, relief = FLAT)
-button_minus.config(image = img, compound = CENTER, relief = FLAT)
-button_divide.config(image = img, compound = CENTER, relief = FLAT)
-button_times.config(image = img, compound = CENTER, relief = FLAT)
-button_square.config(image = img, compound = CENTER, relief = FLAT)
-button_root.config(image = img, compound = CENTER, relief = FLAT)
+        else:
+            e.config(anchor = N)
 
-#puts widgets in window
-frame.grid(row = 0, column = 0, padx = 2, pady = 2)
+    # creating Settings menu
+    set = Toplevel(root)
+    set.title("Settings")
+    set.minsize(180, 122)
+    set.maxsize(180, 122)
+    set.title("Settings")
+    set.iconphoto(True, settingicon)
+
+    # creating widgets
+    descanch = Label(set, relief = FLAT, text = "Text Anchor")
+    version = Label(set, text = "Version: " + str(ver))
+    setting = Button(set, image = settingicon, relief = FLAT, text = "\n\nSave", compound = CENTER, pady = 0, anchor = N, command = save)
+    maker = Label(set, text = "Made By Henry Smith")
+    optionVar = StringVar()
+
+    if e.cget("anchor") == W:
+        optionVar.set("Left     ")
+
+    else:
+        optionVar.set("Middle")
+
+    option = OptionMenu(set, optionVar, "Middle", "Left     ")
+
+    # puts widgets in window
+    maker.grid(row = 0, column = 0, columnspan = 2)
+    version.grid(row = 1, column = 0, columnspan = 2)
+    option.grid(row = 2, column = 1)
+    descanch.grid(row = 2, column = 0)
+    setting.grid(row = 3, column = 0, columnspan = 2)
+
+# creating widgets
+e = Label(root, relief = GROOVE, width = 35, bg = "grey")
+setting = Button(root, image = settingicon, relief = FLAT, command = settings)
+button_1 = Button(root, text = "1", padx = 0, pady = 0, image = img_num, compound = CENTER, relief = FLAT, command = lambda: button_click(1))
+button_2 = Button(root, text = "2", padx = 0, pady = 0, image = img_num, compound = CENTER, relief = FLAT, command = lambda: button_click(2))
+button_3 = Button(root, text = "3", padx = 0, pady = 0, image = img_num, compound = CENTER, relief = FLAT, command = lambda: button_click(3))
+button_4 = Button(root, text = "4", padx = 0, pady = 0, image = img_num, compound = CENTER, relief = FLAT, command = lambda: button_click(4))
+button_5 = Button(root, text = "5", padx = 0, pady = 0, image = img_num, compound = CENTER, relief = FLAT, command = lambda: button_click(5))
+button_6 = Button(root, text = "6", padx = 0, pady = 0, image = img_num, compound = CENTER, relief = FLAT, command = lambda: button_click(6))
+button_7 = Button(root, text = "7", padx = 0, pady = 0, image = img_num, compound = CENTER, relief = FLAT, command = lambda: button_click(7))
+button_8 = Button(root, text = "8", padx = 0, pady = 0, image = img_num, compound = CENTER, relief = FLAT, command = lambda: button_click(8))
+button_9 = Button(root, text = "9", padx = 0, pady = 0, image = img_num, compound = CENTER, relief = FLAT, command = lambda: button_click(9))
+button_0 = Button(root, text = "0", padx = 0, pady = 0, image = img_num, compound = CENTER, relief = FLAT, command = lambda: button_click(0))
+button_equal = Button(root, text = "=", padx = 0, pady = 0, image = img, compound = CENTER, relief = FLAT, command = button_equal)
+button_clear = Button(root, text = "C", padx = 0, pady = 0, image = img, compound = CENTER, relief = FLAT, command = button_clear)
+button_add = Button(root, text = "+", padx = 0, pady = 0, image = img, compound = CENTER, relief = FLAT, command = button_add)
+button_minus = Button(root, text = "-", padx = 0, pady = 0, image = img, compound = CENTER, relief = FLAT, command = button_minus)
+button_divide = Button(root, text = "÷", padx = 0, pady = 0, image = img, compound = CENTER, relief = FLAT, command = button_divide)
+button_times = Button(root, text = "X", padx = 0, pady = 0, image = img, compound = CENTER, relief = FLAT, command = button_times)
+button_square = Button(root, text = "a²", padx = 0, pady = 0, image = img, compound = CENTER, relief = FLAT, command = square)
+button_root = Button(root, text = "√", padx = 0, pady = 0, image = img, compound = CENTER, relief = FLAT, command = rooting)
+
+# puts widgets in window
+setting.grid(row = 7, column = 0, columnspan = 3)
 e.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 10)
-button_1.grid(row = 3,column = 0)
-button_2.grid(row = 3,column = 1)
-button_3.grid(row = 3,column = 2)
+button_1.grid(row = 1,column = 0)
+button_2.grid(row = 1,column = 1)
+button_3.grid(row = 1,column = 2)
 button_4.grid(row = 2,column = 0)
 button_5.grid(row = 2,column = 1)
 button_6.grid(row = 2,column = 2)
-button_7.grid(row = 1,column = 0)
-button_8.grid(row = 1,column = 1)
-button_9.grid(row = 1,column = 2)
+button_7.grid(row = 3,column = 0)
+button_8.grid(row = 3,column = 1)
+button_9.grid(row = 3,column = 2)
 button_0.grid(row = 4,column = 0)
 button_clear.grid(row = 6,column = 0)
 button_equal.grid(row = 5,column = 2)
